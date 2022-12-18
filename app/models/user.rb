49b11d8961972e8c462424b2857da17e91e609a4
austin-darrow <adarrow@utexas.edu>
2022-12-18 11:25:42 -0600

    Add email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /[\w+\-.]+@([a-z0-9]+[.])+[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
 end
